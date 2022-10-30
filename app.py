269d7ac8af887ff42009bcda92c7ff9d72966551
dariomfernandez <74682808+dariomfernandez@users.noreply.github.com>
2022-10-30 08:02:46 -0300

    Initial commit

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import locale
 import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
-import mysql.connector as connection
-import mariadb
+#import mysql.connector as connection
+#import mariadb
 import mysql.connector
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
